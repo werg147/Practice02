d603bb5b5a5ec1ed983eaea89087a12badcd51f8
werg1 <werg1@DESKTOP-9B10C7H>
2020-11-22 16:29:53 +0900

    조건문 연습

--- src/com/javaecx/practice/Ex13.java
+++ src/com/javaecx/practice/Ex13.java
@@ -7,10 +7,21 @@ public class Ex13 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("숫자를 입력해 주세요.");
+		System.out.print("숫자: ");
+		double num = sc.nextDouble();
 		
-		????????
+		double fun;
 		
-		
+		if(num<=0) {
+			fun = (num*num*num) - 9*num + 2;
+			System.out.println("계산 결과는 " + fun + " 입니다.");
+		} else if(num>0) {
+			fun = 7*num + 2;
+			System.out.println("계산 결과는 " + fun + " 입니다.");
+		} 
+				
 		
 		sc.close();
 		
